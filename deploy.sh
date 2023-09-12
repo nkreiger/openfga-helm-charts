acdecd9bd50371b2c73b16f392e391c3a01c34c4
Noah Kreiger <noahkreiger@gmail.com>
2023-09-12 19:48:06 -0400

    add upgrade script

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# IMPORTANT: this must match the chart version at `$CHART_PATH/Chart.yaml`
+CHART_VERSION=0.1.25
+CHART=fianu
+CHART_PATH=charts/openfga
+NAME=openfga
+
+deploy() {
+    helm package $CHART_PATH
+
+    helm gcs push "$NAME-$CHART_VERSION.tgz" $CHART  --force=true
+
+    rm -f "$NAME-$CHART_VERSION.tgz"
+
+    sleep 5
+
+    helm repo update $CHART
+}
+
+deploy
